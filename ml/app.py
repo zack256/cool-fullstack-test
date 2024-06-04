a195d348c9241fa61e298d0beb5c6b0dd1784b89
zack256 <20821071+zack256@users.noreply.github.com>
2024-06-04 01:06:29 -0400

    more on ml container

--- ml/app.py
+++ ml/app.py
@@ -1,2 +1,16 @@
+import keras
+from keras.utils import load_img, img_to_array
+import numpy as np
+import sys
+
+def predict_number(image_filename):
+    model = keras.models.load_model("model.keras")
+    image = load_img(image_filename, color_mode="grayscale", target_size=(28, 28))
+    arr = img_to_array(image)
+    arr /= 255
+    arr = arr.reshape(1, 28, 28, 1)
+    output = model.predict(arr)
+    return np.argmax(output)
+
 if __name__ == "__main__":
-    print("hi!")
+    print(predict_number(sys.argv[1]))
