74872ad46d3b514f58826a9e5584bf8b025babd5
zack256 <20821071+zack256@users.noreply.github.com>
2024-06-10 22:03:52 -0400

    more pages and some error handling

--- web/routes/routes.js
+++ web/routes/routes.js
@@ -8,43 +8,70 @@ const router = Router();
 
 const upload = multer({ dest: "./public/uploads/" });
 
+async function getMongoConnection () {
+    const mongoURI = "mongodb://" + process.env.MONGO_HOST + ":" + process.env.MONGO_PORT;
+    const dbName = process.env.MONGO_DB_NAME;
+    let mongoConnection;
+    try {
+        mongoConnection = new MongoClient(mongoURI);
+        const db = mongoConnection.db(dbName);
+        const fileCollection = db.collection("files");
+        return { mongoConnection, fileCollection };
+    } catch (e) {
+        if (mongoConnection) {
+            await mongoConnection.close();
+        }
+        throw e;
+    }
+}
+
 router.route("/")
     .get(async (req, res) => {
-        res.render("index", {title: "Cool app"});
-    });
+        const {mongoConnection, fileCollection} = await getMongoConnection();
+        let resultsToDisplay;
+        try {
+            let allDocs = await fileCollection.find().toArray();
+            resultsToDisplay = allDocs.map((doc) => {
+                return {
+                    key: doc.key,
+                    result: doc.processed ? doc.result : "still processing!"
+                };
+            });
+        } catch (e) {
+            await mongoConnection.close();
+            throw e;
+        }
+        res.render("index", {
+            title: "Cool app",
+            files: resultsToDisplay
+        });
+    })
 
 router.route("/upload")
+    .get(async (req, res) => {
+        res.render("upload", {title: "Cool app"});
+    })
     .post(upload.single("file"), async (req, res) => {
 
         const filePath = req.file.destination + req.file.filename;
         let keyOfInsertedObject = await uploadFileToBucket(filePath, true);
 
-        const mongoURI = "mongodb://" + process.env.MONGO_HOST + ":" + process.env.MONGO_PORT;
-        const dbName = process.env.MONGO_DB_NAME;
-        const mongoConnection = new MongoClient(mongoURI);
+        const {mongoConnection, fileCollection} = await getMongoConnection();
+        const fileObj = {
+            key: keyOfInsertedObject,
+            processed: false,
+            result: null
+        };
+        
         try {
-            // const mongoConnection = await MongoClient.connect(mongoURI);
-            const db = mongoConnection.db(dbName);
-            const fileCollection = db.collection("files");
-            const fileObj = {
-                key: keyOfInsertedObject,
-                processed: false,
-                result: null
-            };
             const insertionResult = await fileCollection.insertOne(fileObj);
         } catch (e) {
-            console.error(e);
-        } finally {
             await mongoConnection.close();
+            throw e;
         }
 
         res.json("should be inserted!");
 
-    });
-
-router.route("/params")
-    .get(async (req, res) => {
-        return res.json(req.query);
     });
 
 export default router;
